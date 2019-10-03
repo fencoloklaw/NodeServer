49b4eb7bb565f59b0a27d0e72bc1b8c7f67c2ac6
fencoloklaw <fencoloklaw@gmail.com>
2019-10-03 00:36:30 -0700

    connect to db with config file

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # NodeServer
 Node Server application from tutorial : https://code.visualstudio.com/docs/nodejs/nodejs-tutorial
+
--- nodeServerApp/app.js
+++ nodeServerApp/app.js
@@ -3,11 +3,22 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
+var http = require('http');
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var dotenv = require('dotenv');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+
+var db;
 var app = express();
+dotenv.config();
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-jxbyu.mongodb.net/admin?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true });
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -36,6 +47,12 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+client.connect(err => {
+  assert.equal(null, err);
+  console.log('Connected successfully to server');
+  db = client.db
 });
 
 module.exports = app;
